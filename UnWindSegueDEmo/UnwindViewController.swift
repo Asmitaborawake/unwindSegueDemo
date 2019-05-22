07e8214d3e03cd114b7693159fab3f4bac7e63c6
User <user@Users-MacBook-Pro.local>
2019-05-22 17:12:14 +0530

    push code

--- UnWindSegueDEmo/UnwindViewController.swift
+++ UnWindSegueDEmo/UnwindViewController.swift
@@ -8,23 +8,29 @@
 
 import UIKit
 
-class UnwindViewController: UIViewController {
+class UnwindViewController: UIViewController ,UITextFieldDelegate{
 
+    @IBOutlet weak var textField: UITextField!
+    
+    var dataPass : String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return true
     }
-    */
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if let data = textField.text {
+            
+            dataPass = data
+        }
+    }
+
+    
 
 }
